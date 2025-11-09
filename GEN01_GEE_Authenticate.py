1757c734b43873f202d214f5345b81e70957beeb
dllmekongdelta <dll.mekongdelta@gmail.com>
2025-11-09 12:42:12 +0700

    Add files via upload

--- GEN01_GEE_Authenticate.py
+++ GEN01_GEE_Authenticate.py
@@ -7,12 +7,12 @@
 import ee, os
 
 # --------- Activate this before pushing to GitHub --------------------
-# SERVICE_ACCOUNT = os.environ.get("GEE_SERVICE_ACCOUNT")
-# KEY_FILE = "key.json"   # GitHub Actions writes the secret here
+SERVICE_ACCOUNT = os.environ.get("GEE_SERVICE_ACCOUNT")
+KEY_FILE = "key.json"   # GitHub Actions writes the secret here
 
-# credentials = ee.ServiceAccountCredentials(SERVICE_ACCOUNT, KEY_FILE)
-# ee.Initialize(credentials)
+credentials = ee.ServiceAccountCredentials(SERVICE_ACCOUNT, KEY_FILE)
+ee.Initialize(credentials)
 
 # --------- Activate when using in VS Code --------------------
-ee.Authenticate()
-ee.Initialize(project="gee-mekong-map")
+# ee.Authenticate()
+# ee.Initialize(project="gee-mekong-map")
